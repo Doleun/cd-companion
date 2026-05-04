4987d03692d6f60fae098a2b48651fed9c1ab98f
Leandro Diogenes <ldstudio.com@gmail.com>
2026-05-04 00:15:41 -0300

    refactor(inject): create inject_parts, extract bootstrap/state/init

---
+++ overlay/inject_parts/00_bootstrap.js
@@ -0,0 +1,10 @@
+
+(function () {
+  const iv = setInterval(() => {
+    if (!window.isEmbedded) {
+      window.isEmbedded = true;
+      window.dispatchEvent(new Event('resize'))
+    }
+  }, 500);
+  setTimeout(() => clearInterval(iv), 30000);
+})();
